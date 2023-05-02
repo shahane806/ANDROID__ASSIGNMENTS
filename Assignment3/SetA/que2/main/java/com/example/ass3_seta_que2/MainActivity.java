b44e07a6ba7b4a416752695422f2c763f0e9b899
Om Shahane <71377782+shahane806@users.noreply.github.com>
2023-05-02 06:20:03 +0530

    Add files via upload

---
+++ Assignment3/SetA/que2/main/java/com/example/ass3_seta_que2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ass3_seta_que2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText firstname,middlename,lastname,address,email;
+    DatePicker birthday;
+    Button submit;
+    String birth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        firstname = findViewById(R.id.FirstName);
+        middlename = findViewById(R.id.MiddleName);
+        lastname = findViewById(R.id.LastName);
+        address = findViewById(R.id.Address);
+        email = findViewById(R.id.EmailId);
+        birthday = findViewById(R.id.BirthDay);
+        birth = birthday.getDayOfMonth()+": "+birthday.getMonth()+" : "+birthday.getYear();
+        submit = findViewById(R.id.Submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("FirstName",firstname.getText().toString());
+                intent.putExtra("MiddleName",middlename.getText().toString());
+                intent.putExtra("LastName",lastname.getText().toString());
+                intent.putExtra("Address",address.getText().toString());
+                intent.putExtra("EmailId",email.getText().toString());
+                intent.putExtra("BirthDay",birth);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ Assignment3/SetA/que2/main/java/com/example/ass3_seta_que2/MainActivity2.java
@@ -0,0 +1,34 @@
+package com.example.ass3_seta_que2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView textView,textView2,textView3,textView4,textView5,textView6;
+    String first,middle,last,address,birth,email;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+        textView6 = findViewById(R.id.textView6);
+        first = getIntent().getStringExtra("FirstName");
+        middle = getIntent().getStringExtra("MiddleName");
+        last = getIntent().getStringExtra("LastName");
+        address = getIntent().getStringExtra("Address");
+        email = getIntent().getStringExtra("EmailId");
+        birth = getIntent().getStringExtra("BirthDay");
+        textView.setText(first);
+        textView2.setText(middle);
+        textView3.setText(last);
+        textView4.setText(address);
+        textView5.setText(email);
+        textView6.setText(birth);
+    }
+}
